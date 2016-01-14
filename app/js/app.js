02ce20bfbf5f100a1c91009b02374e83eee27bd1
TobiasGustafsson <tobiasgustafsson@live.se>
2016-01-14 14:04:44 +0100

    Markup/css for the login-page

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,7 @@ angular.module('BudgetFriend', [
   'BudgetFriend.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/login', {templateUrl: 'partials/loginpage.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/profile', {templateUrl: 'partials/profilepage.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 }]);
---
+++ app/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   
+    $('#register').on('click', function(){
+       
+       document.getElementById("register-user").style.display = "block";
+        
+    });
+    
+
+});(jQuery)
